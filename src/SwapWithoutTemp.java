1856332e2ec618ab4a15175687f4a416724f0e11
10kagan <kaganalperen@gmail.com>
2020-07-26 23:51:57 -0500

    adding swap method

--- src/SwapWithoutTemp.java
+++ src/SwapWithoutTemp.java
@@ -1,6 +1,14 @@
 public class SwapWithoutTemp {
     public static void main(String[] args) {
 
+        swap(10,20);
+    }
 
+    public static void swap(int num1, int num2){
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println(num1 +" " + num2);
     }
 }
