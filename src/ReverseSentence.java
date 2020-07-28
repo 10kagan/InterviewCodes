40c38ff574e93158dc93db011ab167700493de12
10kagan <kaganalperen@gmail.com>
2020-07-27 23:17:24 -0500

    added ReverseSentence class

---
+++ src/ReverseSentence.java
@@ -0,0 +1,15 @@
+public class ReverseSentence {
+    public static void main(String[] args) {
+        System.out.println(reverse("Tsubasa Ozoro"));
+    }
+
+    public static String reverse(String str){
+        String result = "";
+
+        String [] arr = str.split(" ");
+        for (int i = arr.length -1; i >= 0; i--){
+            result += arr[i] + " ";
+        }
+        return result;
+    }
+}
