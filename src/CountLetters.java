3e8da54aa34117c592b36fd66248b0a40e8f7233
10kagan <kaganalperen@gmail.com>
2020-07-30 00:13:19 -0500

    Added "!"

--- src/CountLetters.java
+++ src/CountLetters.java
@@ -3,14 +3,15 @@ import java.util.Map;
 
 public class CountLetters {
     public static void main(String[] args) {
-        System.out.println(countLetter("ccccccsssccklkk"));
+        System.out.println(countLetter("ahmet ahmed"));
     }
     public static  Map<String, Integer> countLetter(String str){
         Map<String, Integer> map = new HashMap<>();
         String [] strArr = str.split("");
 
+
         for (String each: strArr) {
-            if(map.containsKey(each)){
+            if(!map.containsKey(each)){
                 map.put(each, 1);
             }else
                 map.put(each, each.length() + 1);
@@ -20,3 +21,5 @@ public class CountLetters {
 }
 
 //5.Write a method which returns how many times each letter is repeated?
+//Integer d = (int) map2.get(arr2[j]);
+//                map2.put(arr2[j], ++d);
