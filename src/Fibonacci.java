c1822db580fab79b0280647a83e72bbb7eced26c
10kagan <kaganalperen@gmail.com>
2020-08-01 00:21:50 -0500

    Added Fibonacci.java class

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,18 @@
 public class Fibonacci {
+    public static void main(String[] args) {
+        fib(4);
+    }
+    public static void fib(int num){
+        int result = 0;
+        int a = 0;
+        int b = 1;
+        int c;
+        for (int i = 2; i <= num; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        result = b;
+        System.out.println(result);
+    }
 }
