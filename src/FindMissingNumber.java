a7aa6b5a5857a2e95a50c710effe567785abcf84
10kagan <kaganalperen@gmail.com>
2020-08-02 14:21:05 -0500

    Added FindMissingNumber.java class

---
+++ src/FindMissingNumber.java
@@ -0,0 +1,15 @@
+public class FindMissingNumber {
+    public static void main(String[] args) {
+        System.out.println(findMissingNumber(new int[] {1,2,4,5,6,7,8,9,10}));
+    }
+    public static int findMissingNumber(int [] arr){
+        int result = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] + 1 != arr[i + 1])
+                result = arr[i] + 1;
+        }
+
+        return result;
+    }
+}
