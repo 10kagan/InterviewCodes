7a322fbfd8bdb8562cd0f7f0621c41094d17844c
10kagan <kaganalperen@gmail.com>
2020-08-05 10:09:17 -0500

    Added ShiftAnArray.java class

---
+++ src/ShiftAnArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ShiftAnArray {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5};
+        System.out.println(Arrays.toString(shift(arr1)));
+    }
+    public static int[] shift (int [] arr){
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            temp = arr[i];
+            arr[i] = arr [i + 1];
+            arr[i + 1] = temp;
+        }
+        return arr;
+    }
+}
