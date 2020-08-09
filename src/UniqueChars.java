3972c1b48eec012f5bebab3bf28c4f7006db7678
10kagan <kaganalperen@gmail.com>
2020-08-08 21:39:36 -0500

    Added UniqueChars.java class

---
+++ src/UniqueChars.java
@@ -0,0 +1,15 @@
+public class UniqueChars {
+    public static void main(String[] args) {
+        System.out.println(printUniqueChars("missisipi"));
+
+    }
+    public static String printUniqueChars(String str){
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            String each = str.substring(i, i + 1);
+            if (!result.contains(each))
+                result += each;
+        }
+        return result;
+    }
+}
