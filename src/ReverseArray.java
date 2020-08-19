3062f6e0b38a249007b67a95b2222aca874ca13a
10kagan <kaganalperen@gmail.com>
2020-08-19 10:45:03 -0500

    Adding ReverseArray.java class

---
+++ src/ReverseArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(reverseArray(arr1)));
+    }
+    public static int[] reverseArray(int [] arr){
+        int len = arr.length;
+        int [] rev = new int[len];
+        for (int i = 0; i < len; i++) {
+            rev[i] = arr[len - i - 1];
+        }
+        return rev;
+    }
+}
