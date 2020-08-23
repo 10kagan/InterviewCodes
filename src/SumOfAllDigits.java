1a1a610428309798b45ae9faa6be2e096b4b60a0
10kagan <kaganalperen@gmail.com>
2020-08-23 14:42:24 -0500

    Adding SumOfAllDigits.java class

--- src/SumOfAllDigits.java
+++ src/SumOfAllDigits.java
@@ -1,5 +1,13 @@
 public class SumOfAllDigits {
     public static void main(String[] args) {
-
+        System.out.println(sumDigits(9073));
+    }
+    public static int sumDigits(int num){
+        int result = 0;
+        while (num > 0){
+            result += num % 10;
+            num = num / 10;
+        }
+        return result;
     }
 }
