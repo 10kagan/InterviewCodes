b1eebd911b7a2441b48f67fde54dbfbd3a120445
10kagan <kaganalperen@gmail.com>
2020-08-24 14:49:03 -0500

    Adding Permutation.java class

---
+++ src/Permutation.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutation {
+    public static void main(String[] args) {
+        permutation("1234");
+    }
+    public static void permutation(String str){
+        List<String> lst = new ArrayList<String>();
+
+        for (int i = 0; i < str.length() + 1; i++) {
+            for (int j = 0; j < i; j++){
+                lst.add(str.substring(j, i));
+                System.out.println(j);
+                System.out.println(i);
+            }
+        }
+        System.out.println((lst));
+    }
+}
