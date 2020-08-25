f8c6d8652253abcba522f7fb0f8618491359cedd
10kagan <kaganalperen@gmail.com>
2020-08-24 21:23:10 -0500

    Adding SwapFirstAndLastChar.java class

---
+++ src/SwapFirstAndLastChar.java
@@ -0,0 +1,10 @@
+public class SwapFirstAndLastChar {
+    public static void main(String[] args) {
+        System.out.println(swap("lion"));
+    }
+    public static String swap(String str){
+        return str.substring(str.length() - 1)
+                + str.substring(1, str.length() - 1)
+                + str.substring(0 ,1);
+    }
+}
