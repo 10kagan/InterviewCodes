eeb66f1ba820230858dc5e9601a5ca9422d0d51b
10kagan <kaganalperen@gmail.com>
2022-03-24 23:34:38 -0500

    added SwapWithoutTemp

---
+++ src/part3/_8PrimeNumbers.java
@@ -0,0 +1,12 @@
+package part3;
+
+public class _8PrimeNumbers {
+    public static void main(String[] args) {
+//        int i = 0;
+//        System.out.println(isPrime(i));
+//    }
+//    public static boolean isPrime (int num){
+//
+//        return
+    }
+}
