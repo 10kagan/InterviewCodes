36a249c5854e851d8af131fb9919a85a7ee6b8c1
10kagan <kaganalperen@gmail.com>
2022-03-25 23:36:59 -0500

    added SortAnArray

---
+++ src/part3/_10SortAnArray.java
@@ -0,0 +1,13 @@
+package part3;
+
+import java.util.Arrays;
+
+public class _10SortAnArray {
+    public static void main(String[] args) {
+        int [] arr = {5,2,6,8,-4,0};
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
