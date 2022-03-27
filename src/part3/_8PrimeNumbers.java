ae1d45a01cdb3f7616f772a95e72c68ae8bb2b9f
10kagan <kaganalperen@gmail.com>
2022-03-26 23:52:36 -0500

    added PrimeNumbers

--- src/part3/_8PrimeNumbers.java
+++ src/part3/_8PrimeNumbers.java
@@ -2,11 +2,27 @@ package part3;
 
 public class _8PrimeNumbers {
     public static void main(String[] args) {
-//        int i = 0;
-//        System.out.println(isPrime(i));
-//    }
-//    public static boolean isPrime (int num){
-//
-//        return
+        int i = 7;
+        System.out.println(isPrime(i));
+    }
+    public static boolean isPrime (int num){
+        if(num == 0  || num == 1){
+            return false;
+        }
+        if(num == 2){
+            return true;
+        }
+        for (int i = 2; i <= num/2; i++ ){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
     }
 }
+
+//    public static boolean isPrime(int n) {
+//        for (int i = 2; i <= n / 2; i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
