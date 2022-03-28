f0acad6b1aa7f7693502e787c5dfc77a8aa65ed8
10kagan <kaganalperen@gmail.com>
2022-03-27 21:01:09 -0500

    added HowManyWords.java

--- src/part2/HowManyWords.java
+++ src/part2/HowManyWords.java
@@ -1,5 +1,9 @@
 package part2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class HowManyWords {
     public static void main(String[] args) {
         System.out.println(count("How many words in this sentence, 7?"));
@@ -7,13 +11,18 @@ public class HowManyWords {
     public static int count (String str){
         int result = 0;
         String [] arr = str.split(" ");
-        for(int i = 0; i<= arr.length; i++){
-            //arr[i].replace('?','');
-            if(!arr[i].contains("7"))
+
+        Set<String> wordSet = new HashSet<String>(Arrays.asList(arr));
+        for(int i = 0; i< arr.length; i++){
+            if(!wordSet.add(arr[i])){
                 result++;
+            }
+
+//            if(!arr[i].contains("7"))
+//                result++;
         }
-        
-        
+
+        System.out.println(wordSet);
         return result;
     }
 }
