c06dc34dd6fe80b3b1259ac1ff7d5f017614a577
10kagan <kaganalperen@gmail.com>
2022-03-28 21:51:28 -0500

    added PyramidWithNumbers class

--- src/part3/_2PyramidWithNumbers.java
+++ src/part3/_2PyramidWithNumbers.java
@@ -1,13 +1,35 @@
 package part3;
 
+import java.util.Scanner;
+
 public class _2PyramidWithNumbers {
 
+    public static void printPattern (int num){
+        for (int i = 1; i<= num; i++){
+ //           int numberOfWhiteSpaces = num - 1;
+ //           printString(" ", numberOfWhiteSpaces);
 
-
+            // print numbers
+            printString(i +" ", i);
+            // move to next line
+            System.out.println("");
+        }
+    }
+    // function to print string given times
+    public static void printString(String s, int times){
+        for (int j = 0; j < times; j++){
+            System.out.print(s);
+        }
+    }
 
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter  the rows to print: ");
+        int rows = scanner.nextInt();
+        scanner.close();
 
+        printPattern(rows);
     }
 }
