b595f318462d05a44d171e62e312d5deee90720d
10kagan <kaganalperen@gmail.com>
2022-04-02 00:37:23 -0500

    added PyramidWithNumbers class

--- src/part3/_2PyramidIncreaseAndDecrease.java
+++ src/part3/_2PyramidIncreaseAndDecrease.java
@@ -1,7 +1,34 @@
 package part3;
 
-public class _2PyramidIncreaseAndDecrease {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class _2PyramidIncreaseAndDecrease {
+    public static void printString(String s, int times){
+        for (int j = 0; j < times; j++){
+            System.out.print(s);
+        }
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter  the rows to print: ");
+        int rows = scanner.nextInt();
+        scanner.close();
+        PyramidIncDec(rows);
+    }
+    public static void PyramidIncDec(int rows){
+        for (int i = 1; i <= rows; i++){
+            int numberOfWhiteSpaces = (rows - i) * 2;
+            printString(" ", numberOfWhiteSpaces);
+            for (int k = 1; k <= i -1; k++){
+                System.out.print(k+ " ");
+            }
+            for (int j = i; j >0; j--){
+                System.out.print(j+ " ");
+            }
+            System.out.println("");
+        }
+    }
+
+
+
 }
