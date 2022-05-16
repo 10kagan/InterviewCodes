b75152116b05f6fb1e099f23168094b7458e993c
10kagan <kaganalperen@gmail.com>
2022-05-15 22:36:17 -0500

    added _30LongestCommonPrefix class

---
+++ src/part4/Arrays/_30LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+package part4.Arrays;
+
+public class _30LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strArr={"java2blog","javaworld","javabean","javatemp"};
+        String longestPrefix=getLongestCommonPrefix(strArr);
+        System.out.println("Longest Prefix : "+longestPrefix);
+
+    }
+    public static String getLongestCommonPrefix(String [] strArr){
+        if (strArr.length == 0) return "";
+        String minStr = getMinString(strArr);
+        int minPrefixStringLength = minStr.length();
+        for (int i = 0; i < strArr.length; i++){
+            for (int j = 0; j < minPrefixStringLength; j++){
+                if (minStr.charAt(j) != strArr[i].charAt(j))
+                    break;
+                if (j<minPrefixStringLength)
+                    minPrefixStringLength = j;
+            }
+        }return minStr.substring(0, minPrefixStringLength);
+    }
+
+    private static String getMinString(String[] strArr) {
+        String minStr = strArr[0];
+        for (int i = 1; i < strArr.length; i++){
+            if (strArr[i].length() < minStr.length())
+                minStr = strArr[i];
+        }
+        return minStr;
+    }
+}
