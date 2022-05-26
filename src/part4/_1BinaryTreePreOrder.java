08ac00e12a7e60f404c3cc6c2078a5944a645757
10kagan <kaganalperen@gmail.com>
2022-05-26 00:09:42 -0500

    added _1BinaryTreePreOrder class

--- src/part4/_1BinaryTreePreOrder.java
+++ src/part4/_1BinaryTreePreOrder.java
@@ -1,4 +1,69 @@
 package part4;
 
+import java.util.Stack;
+
 public class _1BinaryTreePreOrder {
+    public static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.data = data;
+        }
+    }
+    public void preorder(TreeNode root){
+        if (root != null){
+            System.out.format("%d", root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    public void preorderIter (TreeNode root){
+        if (root == null)
+            return;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()){
+            TreeNode n = stack.pop();
+            System.out.format("%d", n.data);
+            if(n.right != null)
+                stack.push(n.right);
+            if (n.left != null)
+                stack.push(n.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        _1BinaryTreePreOrder bi = new _1BinaryTreePreOrder();
+        TreeNode rootNode = createBinartTree();
+        System.out.println("Using Recursive solution:");
+
+        bi.preorder(rootNode);
+
+        System.out.println();
+        System.out.println("-------------------------");
+        System.out.println("Using Iterative solution:");
+
+        bi.preorderIter(rootNode);    }
+
+    private static TreeNode createBinartTree() {
+        TreeNode rootNode = new TreeNode(40);
+        TreeNode node20 = new TreeNode(20);
+        TreeNode node10=new TreeNode(10);
+        TreeNode node30=new TreeNode(30);
+        TreeNode node60=new TreeNode(60);
+        TreeNode node50=new TreeNode(50);
+        TreeNode node70=new TreeNode(70);
+
+        rootNode.left = node20;
+        rootNode.right = node60;
+
+        node20.left = node10;
+        node20.right = node30;
+
+        node60.left = node50;
+        node60.right = node70;
+
+        return rootNode;
+    }
 }
