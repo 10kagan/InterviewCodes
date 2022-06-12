df51d0d3717836a4dc1c8e0d9846ce6f08258d6b
10kagan <kaganalperen@gmail.com>
2022-06-11 22:49:50 -0500

    added QuickSort class

---
+++ src/part4/Sorting/QuickSort.java
@@ -0,0 +1,51 @@
+package part4.Sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    private static int array[];
+
+    public static void sort(int[] arr){
+        if (arr == null || arr.length == 0)
+            return;
+        array = arr;
+        quickSort(0, array.length - 1);
+    }
+    private static void quickSort(int left, int right){
+        int i = left;
+        int j = right;
+        int pivot = array[left + (right - left) / 2];
+        while (i <= j){
+            while (array[i] < pivot){
+                i++;
+            }
+            while (array[j] > pivot){
+                j--;
+            }
+            if (i <= j){
+                exchange(i, j);
+                i++;
+                j++;
+            }
+        }
+        if (left < j)
+            quickSort(left, j);
+        if (i < right)
+            quickSort(i, right);
+    }
+    public static void exchange(int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {33,21,45,64,55,34,11,8,3,5,1};
+        System.out.println("Before Sorting : ");
+        System.out.println(Arrays.toString(input));
+        sort(input);
+        System.out.println("==================");
+        System.out.println("After Sorting : ");
+        System.out.println(Arrays.toString(array));
+    }
+}
