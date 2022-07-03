3c46eb2b24436956f255531cb75a655920010a50
10kagan <kaganalperen@gmail.com>
2022-07-03 11:55:28 -0500

    added Fibonacci class

---
+++ src/part5/Fibonacci.java
@@ -0,0 +1,24 @@
+package part5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(9));
+    }
+    static int n1 = 1, n2 = 2, n3 = 0;
+    static List<Integer> fiboList = new ArrayList<>(Arrays.asList(n1, n2));
+
+    public static List<Integer> fibonacci(int count){
+        if (count > 2){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            fiboList.add(n3);
+            fibonacci(--count);
+        }
+        return fiboList;
+    }
+}
