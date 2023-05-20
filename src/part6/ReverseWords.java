4e27d2c4e1264a364a0381cacc791b50957ffad3
10kagan <kaganalperen@gmail.com>
2023-05-20 12:06:36 -0500

    ReverseWords

--- src/part6/ReverseWords.java
+++ src/part6/ReverseWords.java
@@ -3,6 +3,8 @@ package part6;
 import java.util.Arrays;
 
 public class ReverseWords {
+    //bir cumlenin sadece kelime siralamasini reverse yap
+    // bir cumleyi(string) arraye cevir, array reverse yap
     public static void main(String[] args) {
         String sentence = "The house that Jack built";
         System.out.println(Arrays.toString(revSent(sentence)));
