6bba8aa643f875b17b090d5ec52fb1c574d31359
10kagan <kaganalperen@gmail.com>
2023-06-07 14:19:45 -0500

    FindTheMissingNumberInTheArray

---
+++ src/part8_amazon/FindTheMissingNumberInTheArray.java
@@ -0,0 +1,21 @@
+package part8_amazon;
+
+import java.util.Arrays;
+
+public class FindTheMissingNumberInTheArray {
+    public static void main(String[] args) {
+        int [] nums = {3, 7, 1, 2, 8, 4, 5, 10, 6};
+        System.out.println(findMissingNumber(nums));
+    }
+    public static int findMissingNumber (int [] nums){
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length - 1; i++){
+            if (nums[i] + 1 != nums[i + 1]){
+                result = nums[i] + 1;
+                break;
+            }
+        }
+        return result;
+    }
+}
