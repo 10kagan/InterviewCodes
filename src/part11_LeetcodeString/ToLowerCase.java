5779c69c273778621a6419ae9c736daedeef3fab
10kagan <kaganalperen@gmail.com>
2023-07-16 20:35:51 -0500

    ToLowerCase

---
+++ src/part11_LeetcodeString/ToLowerCase.java
@@ -0,0 +1,12 @@
+package part11_LeetcodeString;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String s = "Hello";
+        //Output: "hello"
+        System.out.println(toLowerCase(s));
+    }
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
