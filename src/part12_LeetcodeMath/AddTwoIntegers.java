a25c8706aa3a4972f6d4ba534fe1cc94d96a3b13
10kagan <kaganalperen@gmail.com>
2023-07-17 22:48:43 -0500

    AddTwoIntegers

---
+++ src/part12_LeetcodeMath/AddTwoIntegers.java
@@ -0,0 +1,13 @@
+package part12_LeetcodeMath;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 5;
+//        Output: 17
+//        Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+        System.out.println(sum(num1, num2));
+    }
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
