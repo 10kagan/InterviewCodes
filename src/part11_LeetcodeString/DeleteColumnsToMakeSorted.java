7c0b8723152658bf9c817e51d71b1c6b52aaa717
10kagan <kaganalperen@gmail.com>
2023-07-24 11:08:15 -0500

    DeleteColumnsToMakeSorted

---
+++ src/part11_LeetcodeString/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,33 @@
+package part11_LeetcodeString;
+
+import java.util.Arrays;
+
+public class DeleteColumnsToMakeSorted {
+    public static void main(String[] args) {
+        String [] strs = {"zyx","wvu","tsr"};
+//        Output: 1
+//        Explanation: The grid looks as follows:
+//        cba
+//        daf
+//        ghi
+//        Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+        System.out.println(minDeletionSize(strs));
+    }
+    public static int minDeletionSize(String[] strs) {
+        int count = 0;
+
+        for (int i = 0; i < strs[0].length(); i++){
+            String [] arr = new String[strs.length];{
+                for (int j = 0; j < strs.length; j++){
+                    arr[j] = String.valueOf(strs[i].charAt(j));
+                }
+            }
+            String[] sortedArr = arr.clone();
+            Arrays.sort(sortedArr);
+            if (!Arrays.equals(arr, sortedArr)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
