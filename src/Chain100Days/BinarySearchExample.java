8bdba46de95e535324be5a0ddc0eb9026ce5b3cb
10kagan <kaganalperen@gmail.com>
2025-11-03 22:18:48 -0600

    BinarySearchExample

---
+++ src/Chain100Days/BinarySearchExample.java
@@ -0,0 +1,29 @@
+package Chain100Days;
+
+public class BinarySearchExample {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 9, 11, 13, 15};
+        int target = 4;
+
+        int result = binarySearch(nums, target);
+        System.out.println("Result index: " + result);  // ➜ 4
+    }
+    public static int binarySearch (int [] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] <= target)
+                left = mid + 1;
+            else
+                right =  mid - 1;
+        }
+        return  -1;
+    }
+}
+
+// Implement binary search in a sorted array.
