8a6684e6ae84b798c56d2c8c989f7fe505d2fcf1
10kagan <kaganalperen@gmail.com>
2025-11-08 01:05:01 -0600

    EquilibriumIndexFinder

---
+++ src/Chain100Days/EquilibriumIndexFinder.java
@@ -0,0 +1,33 @@
+package Chain100Days;
+
+public class EquilibriumIndexFinder {
+    public static void main(String[] args) {
+        int[] arr = {-7, 1, 5, 2, -4, 3, 0};
+        int index = findEquilibriumIndex(arr);
+
+        if (index != -1) {
+            System.out.println("Equilibrium index: " + index);
+        } else {
+            System.out.println("No equilibrium index found.");
+        }
+    }
+    public static int findEquilibriumIndex(int [] arr){
+        int totalSum = 0;
+        for (int num: arr){
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            int rightSum = totalSum - leftSum - arr[i];
+
+            if (leftSum == rightSum){
+                return i;
+            }
+            leftSum += arr[i];
+        }
+        return -1; // bulunamadı
+    }
+}
+// Find the equilibrium index in an array.
