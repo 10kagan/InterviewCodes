1af0e9a50207249bfc321b1234d850eac90e7f59
10kagan <kaganalperen@gmail.com>
2025-12-07 21:48:04 -0600

    Singleton

---
+++ src/Chain100Days/Singleton.java
@@ -0,0 +1,21 @@
+package Chain100Days;
+
+public class Singleton {
+
+    public static volatile Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if (instance == null){
+            synchronized (Singleton.class){
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+// Design a singleton class in Java
