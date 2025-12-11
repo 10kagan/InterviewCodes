3aec5444005b31bed2cd6bb15a6815fb5fa3cf05
10kagan <kaganalperen@gmail.com>
2025-12-10 20:58:09 -0600

    Persons

---
+++ src/Chain100Days/Person.java
@@ -0,0 +1,35 @@
+package Chain100Days;
+
+import java.util.Objects;
+
+public class Persons {
+    private int id;
+    private String name;
+
+    public Persons(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Persons person = (Persons) o;
+
+        return id == person.id && Objects.equals(name, person.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString(){
+        return "Persons{" + id + ", " + name + "}";
+    }
+}
